c45e10c6cecfe46c4158007de884625bf65ad90c
Kiana Caston <castonkr@rose-hulman.edu>
2018-03-27 12:32:11 -0400

    finished temp table view

---
+++ MovieQuotes/TempViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TempViewController.swift
+//  MovieQuotes
+//
+//  Created by Kiana Caston on 3/27/18.
+//  Copyright © 2018 Kiana Caston. All rights reserved.
+//
+
+import UIKit
+
+class TempViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TempCell", for: indexPath)
+        
+        // Configure cell with right data
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
