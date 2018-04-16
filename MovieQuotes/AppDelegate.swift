fa180cf84340dbc69e6200bf27c2cb89bc01e47a
Kiana Caston <castonkr@rose-hulman.edu>
2018-04-16 12:25:33 -0400

    added firebase to app

--- MovieQuotes/AppDelegate.swift
+++ MovieQuotes/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
@@ -16,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // TODO: Configure your Firebase project
+        // DONE: Configure your Firebase project
+        FirebaseApp.configure()
         return true
     }
     
